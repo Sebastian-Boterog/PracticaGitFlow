8b0ca7d7f1d049d90df9092b7cef730770f6aea7
JuanSeb8778 <juanseb8778@gmail.com>
2024-10-25 11:33:17 -0500

    ADD sqrt y factorial

--- src/main/java/com/eam/app/Calculadora.java
+++ src/main/java/com/eam/app/Calculadora.java
@@ -22,5 +22,21 @@ public class Calculadora {
 		return a*b;
 	}
 	
-	
+	public static double raizCuadrada(double a) {
+        if (a < 0) {
+            throw new IllegalArgumentException("El número debe ser no negativo");
+        }
+        return Math.sqrt(a);
+    }
+
+	public static int factorial(int a) {
+		if (a < 1) {
+			throw new IllegalArgumentException("El número debe ser no negativo");
+		}
+		int valor = 1;
+		for (int i = a; i >= 1; i--) {
+			valor = valor * i;
+		}
+		return valor;
+	}
 }
